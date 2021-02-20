c424b2705da623d666aab97432cc174525060cca
yuwen chang <provence5058@gmail.com>
2021-02-20 14:57:41 +0800

    create model and set association

---
+++ models/leisurefit.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Leisurefit extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Leisurefit.belongsTo(models.Category)
+      Leisurefit.hasMany(models.Comment)
+      Leisurefit.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'LeisurefitId',
+        as: 'LikedUsers'
+      })
+    }
+  };
+  Leisurefit.init({
+    name: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Leisurefit',
+  });
+  return Leisurefit;
+};
