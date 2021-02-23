a8ba02d0af3c1d3facaa9ad2d3d73a201984b8e9
yuwen chang <provence5058@gmail.com>
2021-02-23 12:48:40 +0800

    feat: add flash-message & modify register

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-require('./models')
+const session = require('express-session')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+require('./models')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
@@ -9,6 +11,18 @@ const PORT = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.success_msg = req.flash('success_msg')
+  next()
+})
 
 app.use(routes)
 
--- controller/userController.js
+++ controller/userController.js
@@ -4,17 +4,31 @@ const User = db.User
 const userController = {
   registerPage: (req, res) => { res.render('register') },
   register: (req, res) => {
-    const { name, email, password } = req.body
-    User.create({
-      name: name,
-      email: email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-    }).then(user => { return res.redirect('/login') })
+    const { name, email, password, passwordConfirm } = req.body
+    if (!password || !name || !email || !passwordConfirm) {
+      req.flash('warning_msg', '所有欄位都是必填。')
+      return res.render('/register', { name, email, password, passwordConfirm })
+    }
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          req.flash('warning_msg', '此email已註冊過。')
+          return res.render('/register', { name, email, password, passwordConfirm })
+        } else if (password !== passwordConfirm) {
+          req.flash('warning_msg', '密碼與確認密碼不相符。')
+          return res.render('/register', { name, email })
+        }
+        User.create({
+          name: name,
+          email: email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+        })
+      }).then(() => res.redirect('/login'))
   },
   logInPage: (req, res) => { res.render('login') },
   logIn: (req, res) => {
     const { email, password } = req.body
-    User.findOne({ where: email })
+    User.findOne({ email })
       .then(user => {
         console.log(user)
         if (!user || user.password !== password) { return res.redirect('back') }
