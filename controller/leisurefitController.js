04eb816fa4f058bdf0b0de5bfc47c867291e1f2e
yuwen chang <provence5058@gmail.com>
2021-04-09 15:23:12 +0800

    feat: add getLeisures

--- controller/leisurefitController.js
+++ controller/leisurefitController.js
@@ -1,6 +1,10 @@
+const db = require('../models')
+const { Leisurefit, Category } = db
+
 const leisurefitController = {
-  getLeisures: (req, res) => {
-    return res.render('index')
+  getLeisures: async (req, res) => {
+    const leisurefits = await Leisurefit.findAll({ raw: true, nest: true, include: [Category] })
+    return res.render('index', { leisurefits })
   },
   googleMap: (req, res) => {
     return res.render('googleMap')
