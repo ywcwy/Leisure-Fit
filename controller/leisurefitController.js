5da44692a9fdccad3672536218a6593f58a0e965
yuwen chang <provence5058@gmail.com>
2021-04-13 14:43:12 +0800

    feat: add pagination

--- controller/leisurefitController.js
+++ controller/leisurefitController.js
@@ -3,24 +3,39 @@ const { Leisurefit, Category, User } = db
 
 const leisurefitController = {
   getLeisurefits: async (req, res) => {
-    const categoryId = req.query.categoryId
     const categories = await Category.findAll({ raw: true })
-    let leisurefits = await Leisurefit.findAll({ raw: true, nest: true, include: [Category, { model: User, as: 'LikedUsers' }] })
-    let category = ''
-    if (categoryId && categoryId !== 'all') {
-      category = await Category.findByPk(Number(categoryId))
-      leisurefits = await Leisurefit.findAll({
-        where: { CategoryId: Number(categoryId) }, raw: true, nest: true, include: [Category, { model: User, as: 'LikedUsers' }]
-      })
+    const itemInPage = 9 // 每頁九筆資料
+    let offset = 0
+    const page = Number(req.query.page) || 1  // 每次使用者點入後會看到的第一頁
+    const whereQuery = {}
+    if (req.query.page) {
+      offset = (req.query.page - 1) * itemInPage
     }
-    leisurefits = leisurefits.map(l => {
+    let categoryId = ''
+    let categoryName = '' // 功能表選單上要顯示的 category 名稱
+    if (req.query.categoryId && req.query.categoryId !== 'all') {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.categoryId = categoryId
+      const category = await Category.findByPk(categoryId)
+      categoryName = category.name
+    }
+
+    let leisurefits = await Leisurefit.findAndCountAll({
+      where: whereQuery, offset, limit: itemInPage, raw: true, nest: true,
+      include: [Category, { model: User, as: 'LikedUsers' }]
+    })
+    const pages = Math.ceil(leisurefits.count / itemInPage) // 最大頁數
+    const totalPage = Array.from({ length: pages }).map((item, index) => index + 1) // 將每個頁數帶入分頁表顯示
+    const prev = page - 1 < 1 ? 1 : page - 1
+    const next = page + 1 > pages ? pages : page + 1
+    leisurefits = leisurefits.rows.map(l => {
       return {
         ...l,
         description: l.description.substring(0, 50) + '...',
         isLiked: req.user ? req.user.LikedLeisurefits.map(d => d.id).includes(l.id) : false
       }
     })
-    return res.render('index', { leisurefits, categories, categoryName: category.name })
+    return res.render('index', { leisurefits, categories, categoryId, categoryName, totalPage, prev, next, page })
   },
   googleMap: (req, res) => {
     return res.render('googleMap', { css: 'googleMap.css', javascript: 'googleMap.js' })
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const passport = require('passport')
 
 router.get('/line', passport.authenticate('line', {
-  scope: ['email', 'profile', 'openid']  // scope的值為我們向line要求的資料
+  scope: ['email', 'profile', 'openid'], // scope的值為我們向line要求的資料
+  successRedirect: '/',
+  failureRedirect: '/login'
 }))
 
 router.get('/line/callback', passport.authenticate('line', {
