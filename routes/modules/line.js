bc285b3ee9ec04aed53188d720955fc89a7a48b1
yuwen chang <provence5058@gmail.com>
2021-05-05 15:01:22 +0800

    revise image message

--- routes/modules/line.js
+++ routes/modules/line.js
@@ -59,13 +59,15 @@ function handleMessage(message, source, replyToken) {
   if (message.type === 'text' && message.text === '課程') {
     if (source.type === 'user') {
       client.replyMessage(replyToken, {
-        type: 'image',
+        type: 'text',
         text: '每週二，一起變強 - 戶外體能。每週三，一起變辣 - 女性限定。每週四，一起變強 - Cross - Fit。每週課表請詳連結 https://leisure-fit.herokuapp.com/calendar'
-      })
-        .catch(err => console.log(err))
+      }, {
+        type: 'image',
+        originalContentUrl: 'https://i.imgur.com/qzzXpU1.png',
+        previewImageUrl: 'https://i.imgur.com/qzzXpU1.png'
+      }).catch(err => console.log(err))
     }
   }
-
 }
 
 
