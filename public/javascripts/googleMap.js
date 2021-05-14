4382d89b0dabc114365f594a94bac69613eefb60
yuwen chang <provence5058@gmail.com>
2021-05-14 13:05:51 +0800

    modify google map

--- public/javascripts/googleMap.js
+++ public/javascripts/googleMap.js
@@ -5,8 +5,8 @@ function initMap() {
     { lat: 25.051801936560064, lng: 121.55047377575801 } // 小巨蛋
   ]
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 25.033207446034734, lng: 121.54357533989239 },
-    zoom: 13,
+    center: { lat: 25.042589, lng: 121.543984 },
+    zoom: 12,
   })
 
   const locationInfo = [`<div id="content">週二週四-台大操場</div>`, `<div id="content">雨備場地-辛亥橋下</div>`, `<div id="content">週一瑜珈-不限男女、週三限女-一起變辣</div>`]
