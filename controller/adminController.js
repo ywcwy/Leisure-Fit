c7d2774fb9b10f7b0430c5a2197730cb04b52cd7
yuwen chang <provence5058@gmail.com>
2021-05-14 12:22:31 +0800

    modify adminController & leisurefit page

--- controller/adminController.js
+++ controller/adminController.js
@@ -7,69 +7,82 @@ client.on('uploadProgress', (progress) => console.log(progress))
 
 const adminController = {
   getLeisurefits: async (req, res) => {
-    try {
-      let leisurefits = await Leisurefit.findAll({ raw: true, nest: true, include: [Category] })
-      leisurefits = await Promise.all(leisurefits.map(async (l) => {
-        const { count } = await Like.findAndCountAll({ where: { LeisurefitId: l.id } })
-        return {
-          ...l,
-          createdAt: formatDate(l.createdAt),
-          likes: count
-        }
-      }))
-      return res.render('admin/leisurefits', { leisurefits })
-    } catch (error) { console.log(error) }
+    let leisurefits = await Leisurefit.findAll({ raw: true, nest: true, include: [Category] })
+    leisurefits = await Promise.all(leisurefits.map(async (l) => {
+      const { count } = await Like.findAndCountAll({ where: { LeisurefitId: l.id } })
+      return {
+        ...l,
+        createdAt: formatDate(l.createdAt),
+        likes: count
+      }
+    }))
+    return res.render('admin/leisurefits', { leisurefits })
   },
   createLeisurefit: async (req, res) => {
-    try {
-      const categories = await Category.findAll({ raw: true })
-      const leisurefit = await Leisurefit.findByPk(req.params.id, { raw: true })
-      return res.render('admin/leisurefitCreate', { categories, leisurefit })
-    } catch (error) { console.log(error) }
+    const categories = await Category.findAll({ raw: true })
+    const leisurefit = await Leisurefit.findByPk(req.params.id, { raw: true })
+    return res.render('admin/leisurefitCreate', { categories, leisurefit })
   },
   postLeisurefit: async (req, res) => {
+    let { categoryId, name, description } = req.body
+    let img = ''
+    if (req.file) {
+      try {
+        img = await client.upload(req.file.path)
+      } catch (error) {
+        req.flash('warning_msg', '照片上傳失敗')
+        return res.redirect('back')
+      }
+    }
     try {
-      let { categoryId, name, description } = req.body
-      let img = ''
-      if (req.file) { img = await client.upload(req.file.path) }
-      // console.log('********')
-      // console.log(description)
-      // description = description.replace('\n', '<br />')
       await Leisurefit.create({ name, CategoryId: Number(categoryId), description, image: req.file ? img.data.link : '' })
-      req.flash('success_msg', '貼文新增成功')
-      return res.redirect(`/admin/leisurefits`)
-    } catch (error) { console.log(error) }
+    } catch (error) {
+      req.flash('warning_msg', 'po文失敗')
+      return res.redirect('back')
+    }
+
+    req.flash('success_msg', '貼文新增成功')
+    return res.redirect(`/admin/leisurefits`)
   },
   getLeisurefit: async (req, res) => {
-    try {
-      const leisurefit = await Leisurefit.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
-      leisurefit.createdAt = formatDate(leisurefit.createdAt)
-      leisurefit.description = leisurefit.description.replace(/\r?\n/g, '<br />')
-      return res.render('admin/leisurefit', { leisurefit })
-    } catch (error) { console.log(error) }
+    const leisurefit = await Leisurefit.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
+    leisurefit.createdAt = formatDate(leisurefit.createdAt)
+    leisurefit.description = leisurefit.description.replace(/\r?\n/g, '<br />')
+    return res.render('admin/leisurefit', { leisurefit })
   },
   putLeisurefit: async (req, res) => {
+
+    const leisurefit = await Leisurefit.findByPk(req.params.id, { include: [Category] })
+    let { categoryId, name, description } = req.body
+    let img = leisurefit.image
+    if (req.file) {
+      try {
+        img = await client.upload(req.file.path)
+      } catch (error) {
+        req.flash('warning_msg', '照片上傳失敗')
+        return res.redirect('back')
+      }
+    }
+
     try {
-      const leisurefit = await Leisurefit.findByPk(req.params.id, { include: [Category] })
-      let { categoryId, name, description } = req.body
-      // console.log('********')
-      // console.log(description)
-      // description = description.replace(/\n/g, '<br />')
-      // console.log('^^^^^^^^^^^')
-      // console.log(description)
-      let img = leisurefit.image
-      if (req.file) { img = await client.upload(req.file.path) }
       await leisurefit.update({ name, CategoryId: Number(categoryId), description, image: req.file ? img.data.link : leisurefit.image })
-      req.flash('success_msg', '貼文編輯成功')
-      return res.redirect(`/admin/leisurefits/${req.params.id}`)
-    } catch (error) { console.log(error) }
+    } catch (error) {
+      req.flash('warning_msg', '編輯儲存失敗')
+      return res.redirect('back')
+    }
+
+    req.flash('success_msg', '貼文編輯成功')
+    return res.redirect(`/admin/leisurefits/${req.params.id}`)
   },
   deleteLeisurefit: async (req, res) => {
     try {
       await Leisurefit.destroy({ where: { id: req.params.id } })
-      req.flash('success_msg', '貼文刪除成功')
-      return res.redirect('/admin/leisurefits')
-    } catch (error) { console.log(error) }
+    } catch (error) {
+      req.flash('warning_msg', '貼文刪除失敗')
+      return res.redirect('back')
+    }
+    req.flash('success_msg', '貼文刪除成功')
+    return res.redirect('/admin/leisurefits')
   }
 }
 
